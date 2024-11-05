7134c9287847bf7ed0bedeae6093e1b7b0916b97
sidney <sidney@jigsaw.nl>
2024-11-05 08:37:41 +0100

    Improved test program.

--- reciter_test/reciter_python_test.py
+++ reciter_test/reciter_python_test.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env -S python3 -B
 
-"""This is a tester for the Python re-implementation of the SAM Reciter program."""
+"""This is a test tool for the Python re-implementation of the SAM Reciter program."""
 
 import argparse
 import re
@@ -9,38 +9,24 @@ from reciter_python import Reciter
 from reciter_python_rewrite_rules import read_reciter_rules_into_dictionary
 
 
-def main():
-    """Main test function."""
+def test_reciter_with_testcase_file(reciter: Reciter, filename: str):
+    """Feed testcases from a file into the reciter and report results.
 
-    # pylint: disable=too-many-locals
+    The file must contain lines of the form:
 
-    # Parse command line arguments.
+    {SOURCE_STRING} -> {SAM_PHONEMES_STRING}
 
-    default_testcase_filename = "tests/reciter_features.out"
-    default_rules_filename = "english_reciter_rules.txt"
+    The curly braces should be present as literal characters. They serve to precisely
+    indicate the beginning and end of both the SOURCE_STRING and SAM_PHONEMES_STRING,
+    both of which may start or end with whitespace characters.
 
-    parser = argparse.ArgumentParser(description="Test the Python re-implementation of the SAM Reciter.")
-
-    parser.add_argument("-f", "--filename", default=default_testcase_filename,
-                        help=f"testcase filename (default: {default_testcase_filename})")
-    parser.add_argument("--rules-filename", default=default_rules_filename,
-                        help=f"rewrite rule definition filename (default: {default_rules_filename})")
-    parser.add_argument("--fix-bugs", action='store_true',
-                        help="fix known bugs in the rewrite rule matching")
-
-    args = parser.parse_args()
-
-    # Read the rewrite rule file into a rewrite rule dictionary.
-
-    reciter_rules_dictionary = read_reciter_rules_into_dictionary(args.rules_filename)
-
-    # Make a Reciter instance that will use the rewrite rule dictionary we just read.
-
-    reciter = Reciter(reciter_rules_dictionary, fix_bugs=args.fix_bugs)
+    The SOURCE_STRING will be translated to SAM phonemes by passing it to
+    the Reciter. The result will be compared with the SAM_PHONEMES_STRING.
+    """
 
     # Read the testcases.
 
-    with open(args.filename, "r", encoding="ascii") as fi:
+    with open(filename, "r", encoding="ascii") as fi:
         testcases = fi.read().splitlines()
 
     # Process the testcases.
@@ -73,6 +59,36 @@ def main():
     print(f"Python Reciter tests: success = {success_count}, failure = {failure_count}"
           f" ({success_count/(success_count+failure_count)*100.0:.2f}%).")
     print()
+
+
+def main():
+    """Test the Python Reciter ."""
+
+    # Parse command line arguments.
+
+    default_testcase_filename = "tests/reciter_features.out"
+    default_rules_filename = "english_reciter_rules.txt"
+
+    parser = argparse.ArgumentParser(description="Test the Python re-implementation of the SAM Reciter.")
+
+    parser.add_argument("-f", "--filename", default=default_testcase_filename,
+                        help=f"testcase filename (default: {default_testcase_filename})")
+    parser.add_argument("--rules-filename", default=default_rules_filename,
+                        help=f"rewrite rule definition filename (default: {default_rules_filename})")
+    parser.add_argument("--fix-bugs", action='store_true',
+                        help="fix known bugs in the rewrite rule matching")
+
+    args = parser.parse_args()
+
+    # Read the rewrite rule file into a rewrite rule dictionary, and make a Reciter instance
+    # that will use those rules.
+
+    reciter_rules_dictionary = read_reciter_rules_into_dictionary(args.rules_filename)
+    reciter = Reciter(reciter_rules_dictionary, fix_bugs=args.fix_bugs)
+
+    # Test the Reciter against testcases read from a file.
+
+    test_reciter_with_testcase_file(reciter, args.filename)
 
 
 if __name__ == "__main__":
